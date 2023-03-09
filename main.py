d4e18e904d5a29c5d909611dc997dd04cb413351
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2023-03-09 11:17:38 +0700

    for test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from change import check_files_for_deploy
 class run_main:
     
     # Can specify deploy date
-    date = "2023-02-27"
+    date = "2023-03-10"
     # Can specify storage 
     storage = "scbedwseasta001adlsuat"
     # Can specify container 
