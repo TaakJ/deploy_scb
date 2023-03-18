10bcda6cf53a069492b0bc7245bbb5ae5c634a4e
Thanakit Jitcharoenjai <smild_dream@hotmail.com>
2023-03-18 09:14:47 +0700

    for test

--- main.py
+++ main.py
@@ -17,20 +17,32 @@ class run_main:
     # Can specify container 
     container = "edw-ctn-landing"
     
-    path = os.getcwd() + f'/output/{date}'
+    # path
+    path_main  = os.getcwd()
+    path_output = path_main + f'/output/{date}'
+    path_table_def = path_main + f'/filename/U02_TABLE_DEFINITION/{date}'
+    os.makedirs(path_table_def,exist_ok=True)
+    path_int_mapp = path_main + f'/filename/U03_INT_MAPPING/{date}'
+    os.makedirs(path_int_mapp,exist_ok=True)
+    path_pl_register = path_main + f'/filename/U99_PL_REGISTER_CONFIG/{date}'
+    os.makedirs(path_pl_register,exist_ok=True)
+    path_pl_ddl = path_main + f'/filename/DDL/{date}'
+    os.makedirs(path_pl_ddl,exist_ok=True)
     
     t1 = time.time()
     ## start split parameter
-    print("start split file to parameter ..")
+    print("start function split file to parameter ..")
     run_process_split(date=date, re_deploy=re_deploy).run()
     
     ## start merge 
-    run_process_merge(date=date, storage=storage, container=container, re_deploy=re_deploy)
-    
+    if any(os.scandir(path_table_def)) and any(os.scandir(path_int_mapp)) and any(os.scandir(path_pl_register)) and any(os.scandir(path_pl_ddl)):
+        print("start function merge file ..")
+        run_process_merge(date=date, storage=storage, container=container, re_deploy=re_deploy)
+
     ## start genfile
-    if glob.glob(f'{path}/deployment_checklist_{date_fmt}.xlsx'):
-        print("start gen file ..")
-        run_process_genfile(date=date)
+    # if glob.glob(f'{path_output}/deployment_checklist_{date_fmt}.xlsx'):
+    #     print("start gen file ..")
+    #     run_process_genfile(date=date)
     
     t2 = time.time() - t1
     print(f'Executed in {t2:0.2f} seconds.')
